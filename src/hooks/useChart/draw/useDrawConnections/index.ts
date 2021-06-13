f22b037fdf552620acaae95ba8496d3b09719f53
Sergio Ivanuzzo <sergio.ivanuzzo@gmail.com>
2021-06-13 13:08:38 +0300

    added arrows

--- src/hooks/useChart/draw/useDrawConnections/index.ts
+++ src/hooks/useChart/draw/useDrawConnections/index.ts
@@ -1,22 +1,17 @@
 import { useCallback } from "react";
 import * as d3 from "d3";
 
-import getRange from "~/helpers/getRange";
-
 import theme from "~/common/theme";
 
 const MIN_DISTANCE = 300;
-
-// node size
-const RADIUS = 10;
-
-const OFFSET_X = 50;
-const OFFSET_Y = 50;
 
 const MARGIN_LEFT = 30;
 const MARGIN_RIGHT = 30;
 const MARGIN_TOP = 30;
 const MARGIN_BOTTOM = 30;
+
+const NODE_RECT_WIDTH = 100;
+const NODE_RECT_HEIGHT = 150;
 
 const { orange } = theme.palette;
 
@@ -26,7 +21,7 @@ const useDrawConnections = (
 
     // https://stackoverflow.com/a/18473154/5397119
     const polarToCartesian = (centerX: number, centerY: number, radius: number, angleInDegrees: number) => {
-        const angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+        const angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
 
         return {
             x: centerX + (radius * Math.cos(angleInRadians)),
@@ -35,26 +30,21 @@ const useDrawConnections = (
     }
 
     const describeArc = (
-        startX: number,
-        startY: number,
-        endX: number,
-        endY: number,
+        x: number,
+        y: number,
         radius: number,
         startAngle: number,
         endAngle: number
     ) => {
-
-        const start = polarToCartesian(startX, startY, radius, endAngle);
-        const end = polarToCartesian(endX, endY, radius, startAngle);
+        const start = polarToCartesian(x, y, radius, endAngle);
+        const end = polarToCartesian(x, y, radius, startAngle);
 
         const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
 
-        const d = [
+        return [
             "M", start.x, start.y,
             "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y,
         ].join(" ");
-
-        return d;
     }
 
     const draw = useCallback(({ chartRef, width, height }: IChartDrawProps): void => {
@@ -64,12 +54,30 @@ const useDrawConnections = (
             .attr("height", height)
             .attr("width", width);
 
+        const marker = svg.append("defs").append("marker");
+        marker
+            .attr("id", "arrowhead")
+            .attr("viewBox", "0 -5 10 10")
+            .attr("refX", 6)
+            .attr("refY", 0)
+            .attr("markerWidth", 3)
+            .attr("markerHeight", 3)
+            .attr("orient", "auto-start-reverse")
+            .attr("markerUnits", "strokeWidth")
+            .append("path")
+            .attr("d", "M0,-5L10,0L0,5")
+
+        marker
+            .append("path")
+            .attr("d", "M 0,-5 L 10 ,0 L 0,5")
+            .attr("fill", orange.carrot);
+
         const pairAmount = edges.length / 2;
         const itemWidth = (width - MARGIN_LEFT - MARGIN_RIGHT) / pairAmount;
         const connections = Array.from({ length: pairAmount }).map((_, i: number) => {
             return {
                 top: [ MARGIN_LEFT + itemWidth * i, MARGIN_TOP ],
-                bottom: [ MARGIN_LEFT + itemWidth * i, 300 - MARGIN_BOTTOM ]
+                bottom: [ MARGIN_LEFT + itemWidth * i, height - MARGIN_BOTTOM ]
             }
         });
 
@@ -85,10 +93,10 @@ const useDrawConnections = (
             }
         });
 
-        const simulation: any = d3.forceSimulation().nodes(nodes as any).velocityDecay(0.1)
+        const simulation: any = d3.forceSimulation().nodes(nodes as any)
             .force("link", d3.forceLink().id((d: any) => d.id).distance(MIN_DISTANCE))
-            .force("charge", d3.forceManyBody())
-            // .force("x", d3.forceX(width / 2).strength(0.00015))
+            // .force("charge", d3.forceManyBody())
+            // .force("x", d3.forceX(width / 2).strength(0.0015))
             // .force("y", d3.forceY(height / 2).strength(0.15))
             .force("center", d3.forceCenter(width / 2, height / 2));
 
@@ -106,8 +114,22 @@ const useDrawConnections = (
             .data(edges as any)
             .enter()
             .append("path")
-            .attr("d", (d: any) => describeArc(d.source.x, d.source.y, d.target.x, d.target.y, 1, 30, 150))
+            .attr("d", (d: any, i: number) => {
+                const radius = 120;
+                const startAngle = 30;
+                const endAngle = 150;
+
+                return describeArc(
+                    i % 2 === 0 ? d.source.x - NODE_RECT_WIDTH : d.source.x + NODE_RECT_WIDTH,
+                    (i % 2 === 0 ? d.source.y : d.target.y) + NODE_RECT_HEIGHT,
+                    radius,
+                    i % 2 === 0 ? endAngle : startAngle,
+                    i % 2 === 0 ? startAngle : endAngle,
+                );
+            })
             .attr("fill", "none")
+            .attr("transform", "translate(100, 100) scaleX(25)")
+            .attr("marker-start","url(#arrowhead)")
             .attr("stroke", orange.carrot)
             .attr("stroke-width", (d: any) => d.weight);
 
@@ -117,21 +139,12 @@ const useDrawConnections = (
             .append("g")
             .attr("class", "nodes");
 
-        const rectWidth = 150;
-        const rectHeight = 150;
-
         node.append("rect")
-            .attr("x", (d: any) => d.x - rectWidth / 2)
-            .attr("y", (d: any) => d.y - rectHeight / 2)
-            // .attr("cx", (d: any) => d.x)
-            // .attr("cy", (d: any) => d.y)
-            .attr("fill", "white")
-            .attr("width", rectWidth)
-            .attr("height", rectHeight);
-            // .attr("rx", 100)
-            // .attr("ry", 40)
-            // .attr("fill", "white")
-            // .attr("r", RADIUS * 5);
+            .attr("x", (d: any) => d.x - NODE_RECT_WIDTH / 2)
+            .attr("y", (d: any) => d.y - NODE_RECT_HEIGHT / 2)
+            // .attr("fill", "none")
+            .attr("width", NODE_RECT_WIDTH)
+            .attr("height", NODE_RECT_HEIGHT);
 
         node.append("text")
             .text((d: any) => d.name)
