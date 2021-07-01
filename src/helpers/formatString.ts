bda4955dbee91c76ceb35a1c088de388895f81b1
Sergio Ivanuzzo <sergio.ivanuzzo@gmail.com>
2021-07-02 00:37:37 +0300

    added compatibility for browsers that not support replaceAll

--- src/helpers/formatString.ts
+++ src/helpers/formatString.ts
@@ -4,11 +4,32 @@ interface IFormatStringProps {
     params?: string[];
 }
 
+const replaceAll = (input: string, from: string, to: string) => {
+    if (input.replaceAll) {
+        return input.replaceAll(from, to);
+    } else {
+        while (input.includes(from)) {
+            input = input.replace(from, to);
+        }
+        return input;
+    }
+};
+
 const formatString = ({ initial, replacer = (param: string) => param, params = [] }: IFormatStringProps): string => {
     return params?.length ? params.reduce(
-        (result, param: string, index: number) => result.replaceAll(`{${index}}`, replacer(param)),
+        (result, param: string, index: number) => replaceAll(
+            result,
+            `{${index}}`,
+            replacer(param)),
         initial
     ) : initial;
 };
+
+// const formatString = ({ initial, replacer = (param: string) => param, params = [] }: IFormatStringProps): string => {
+//     return params?.length ? params.reduce(
+//         (result, param: string, index: number) => result.replaceAll(`{${index}}`, replacer(param)),
+//         initial
+//     ) : initial;
+// };
 
 export default formatString;
