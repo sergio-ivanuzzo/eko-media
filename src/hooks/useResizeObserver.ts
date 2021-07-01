2c11a8661e5d06323e2f741b71e40ff72786b7c7
Sergio Ivanuzzo <sergio.ivanuzzo@gmail.com>
2021-07-02 00:50:30 +0300

    added compatibility for browsers that not support resizeObserver

--- src/hooks/useResizeObserver.ts
+++ src/hooks/useResizeObserver.ts
@@ -1,9 +1,10 @@
+import { ResizeObserver as Observer } from "resize-observer";
 import { useEffect } from "react";
 
 const useResizeObserver = <T extends Element>(target: T | null, onResize: Function): void => {
     useEffect(() => {
         if (target) {
-            const observer = new ResizeObserver(() => {
+            const observer = new (window.ResizeObserver ?? Observer)(() => {
                 const { height, width } = target.getBoundingClientRect();
                 onResize({ height, width });
             });
