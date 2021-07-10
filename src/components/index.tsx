88b8b4c9062b1778a5ea2ab6d7f0bcc8f04fd9a6
Sergio Ivanuzzo <sergio.ivanuzzo@gmail.com>
2021-07-10 22:06:18 +0300

    experiment with routing

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,11 +1,22 @@
-import React from "react";
 import { ToastContainer } from "react-toastify";
+import React, { useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
 import Content from "~/components/partials/Content";
 import Footer from "~/components/partials/Footer";
 import Header from "~/components/partials/Header";
 
 const App = (): JSX.Element => {
+    const history = useHistory();
+    const location = useLocation();
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log(location.pathname);
+            history.push(location.pathname);
+        }, 0);
+    }, []);
+
     return (
         <>
             <Header sticky />
